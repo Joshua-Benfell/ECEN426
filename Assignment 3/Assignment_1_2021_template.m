5fb39122e753a7eacca62906e82db85f29504fa1
Josh Benfell <joshuabenfell@hotmail.co.nz>
2021-10-20 22:20:01 +1300

    A3 done

--- Assignment 3/Assignment_1_2021_template.m
+++ Assignment 3/Assignment_1_2021_template.m
@@ -46,16 +46,18 @@ x0 = [-2, 0, 10]'; % Don't change this - it is the true initial state that you
 
 x(:,1) = x0; % Set the initial state for the simulation.
 
-% Our initial guess at the neck position.
-x_post(:,1) = [?,?,?]';     % Put your initial estimate here. 
-P_post = diag([?,?,?]);     % Put your initial estimate here. 
+% Our initial guess at the neck position. a)
+x_post(:,1) = [0,0,0]';     % Put your initial estimate here. 
+P_post = diag([5,5,5]);     % Put your initial estimate here. 
 
 % %-----------------------------------------------------------------------------
-% % Run simulation 
+L_acc = zeros(length(t),3);
+L_acc(1,:) = [P_post * C_1' * inv( C_1 * P_post *C_1' + R_1)].';  % initialise the accumulated L with initial guess
+% % Run simulation b)
 
 I=eye(3);
 
-for k=2:length(t);
+for k=2:length(t)
   % x(k) = Ax(k-1) + Bu(k) + w(k)
   w = G*sqrt(var_acceleration)*randn(1,1);
 
@@ -74,6 +76,7 @@ for k=2:length(t);
     y(:,k) = C_1*x(:,k) + v;         % Measure the state.
 
     L = P_prior * C_1' * inv( C_1 * P_prior *C_1' + R_1);
+    L_acc(k,:) = L;
     x_post(:,k) = x_prior(:,k) + L*(y(:,k)-C_1*x_prior(:,k));
     P_post = (I-L*C_1)*P_prior;
     P_post = 0.5*(P_post + P_post'); % Make sure that the covariance remains symmetric.
@@ -82,10 +85,119 @@ for k=2:length(t);
 end
 
 % Plot results -----------------------------------------------------------------
-for plot_num = 1:3,
+figure(1)
+for plot_num = 1:3
   subplot(3,1,plot_num)
   stairs(t,[x(plot_num,:);x_post(plot_num,:)]')
   ylabel(['x_' num2str(plot_num)])
+  legend("x","x posterior")
+end
+xlabel('Time (s)')
+
+% B
+figure(2)
+plot(t,L_acc)
+ylabel("L")
+xlabel("Time (s)")
+legend("x1","x2","x3")
+title("Startup Transients")
+
+%C Steady State Kalman Filter
+
+%L is constant and set to the last value of L in the non-steady state run
+%through
+
+L_ss = L;
+x_ss(:,1) = x0; % Set the initial state for the simulation.
+x_post_ss(:,1) = [0,0,0]';     % Put your initial estimate here. 
+
+for k=2:length(t)
+  % x(k) = Ax(k-1) + Bu(k) + w(k)
+  w = G*sqrt(var_acceleration)*randn(1,1);
+
+  x_ss(:,k) = A*x_ss(:,k-1) + w;   % u=0, so omit the B*u(k) term.
+  
+  x_prior_ss(:,k) = A*x_post_ss(:,k-1);
+  
+  % Update the state using Sensor One at every step. 
+  % If you were to change S1_update_interval to 3 for example, then this block
+  % of code would only run every third time through the outer loop.
+
+  S1_update_interval = 1; 
+  if mod(k, S1_update_interval)==0 
+    v = sqrt(R_1)*randn(1,1); % Find noise to add to the measurement.
+    y(:,k) = C_1*x_ss(:,k) + v;         % Measure the state.
+
+    x_post_ss(:,k) = x_prior_ss(:,k) + L_ss*(y(:,k)-C_1*x_prior_ss(:,k));
+  end
+  
+end
+
+% Plot results -----------------------------------------------------------------
+figure(3)
+for plot_num = 1:3
+  subplot(3,1,plot_num)
+  stairs(t,[x_ss(plot_num,:);x_post_ss(plot_num,:)]')
+  ylabel(['x_' num2str(plot_num)])
+  legend("x","x posterior")
+end
+xlabel('Time (s)')
+%Is the same because the non-steady-state filter quickly settles on L
+
+% D Add a second sensor
+
+x_sen(:,1) = x0; % Set the initial state for the simulation.
+% Our initial guess at the neck position. a)
+x_post_sen(:,1) = [0,0,0]';     % Put your initial estimate here. 
+P_post_sen = diag([5,5,5]);     % Put your initial estimate here. 
+
+for k=2:length(t)
+  % x(k) = Ax(k-1) + Bu(k) + w(k)
+  w = G*sqrt(var_acceleration)*randn(1,1);
+
+  x_sen(:,k) = A*x_sen(:,k-1) + w;   % u=0, so omit the B*u(k) term.
+  
+  x_prior_sen(:,k) = A*x_post_sen(:,k-1);
+  P_prior_sen = A*P_post_sen*A' + Q;
+  
+  % Update the state using Sensor One at every step. 
+  % If you were to change S1_update_interval to 3 for example, then this block
+  % of code would only run every third time through the outer loop.
+
+  S1_update_interval = 1; 
+  S2_update_interval = 30;
+  if mod(k, S1_update_interval)==0 
+    v = sqrt(R_1)*randn(1,1); % Find noise to add to the measurement.
+    if mod(k, S2_update_interval) == 0
+        v2 = sqrt(R_2)*randn(1,1);
+        
+        y(:,k) = C_2*x_sen(:,k) + v2;         % Measure the state.
+        L_sen = P_prior_sen * C_2' * inv( C_2 * P_prior_sen *C_2' + R_2);
+        L_acc_sen(k,:) = L_sen;
+        x_post_sen(:,k) = x_prior_sen(:,k) + L_sen*(y(:,k)-C_2*x_prior_sen(:,k));
+        P_post_sen = (I-L_sen*C_2)*P_prior_sen;
+        P_post_sen = 0.5*(P_post_sen + P_post_sen'); % Make sure that the covariance remains symmetric.
+
+    else
+        y(:,k) = C_1*x_sen(:,k) + v;         % Measure the state.
+
+        L_sen = P_prior_sen * C_1' * inv( C_1 * P_prior_sen *C_1' + R_1);
+        L_acc_sen(k,:) = L_sen;
+        x_post_sen(:,k) = x_prior_sen(:,k) + L_sen*(y(:,k)-C_1*x_prior_sen(:,k));
+        P_post_sen = (I-L_sen*C_1)*P_prior_sen;
+        P_post_sen = 0.5*(P_post_sen + P_post_sen'); % Make sure that the covariance remains symmetric.
+    end
+  end
+  
+  
+  
+end
+figure(4)
+for plot_num = 1:3
+  subplot(3,1,plot_num)
+  stairs(t,[x_sen(plot_num,:);x_post_sen(plot_num,:)]')
+  ylabel(['x_' num2str(plot_num)])
+  legend("x","x posterior")
 end
 xlabel('Time (s)')
 
